ac549fbc0e84b0f03a1a16d15579d185daaa1638
ianbelfatto <ibelfatto@gmail.com>
2021-06-18 14:47:59 -0400

    renames controller

--- app/controllers/businesses_controller.rb
+++
@@ -1,7 +0,0 @@
-require "http"
-class BusinessesController < ApplicationController
-  def index
-    response = HTTP.get("https://api.yelp.com/v3/businesses/search?term=bar, vegan&location=106 Maiden Lane&radius=1609")
-    render json: response.as_json
-  end
-end
---
+++ app/controllers/trip_businesses_controller.rb
@@ -0,0 +1,2 @@
+class TripBusinessesController < ApplicationController
+end
