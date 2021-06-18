c280d247cbfa3291144b3f8bee50e0d345834633
ianbelfatto <ibelfatto@gmail.com>
2021-06-18 15:22:40 -0400

    adds api key secure credentials

--- app/models/trip_business.rb
+++ app/models/trip_business.rb
@@ -4,7 +4,7 @@ class TripBusiness < ApplicationRecord
   def business
     # logic to query third party api for the correct book
     # get back object or hash of book data
-    response = HTTP.auth("Bearer LKjWeyV15MlmLe-LSTDP1VSzKafBmcmas6K1trP8lD3R_BjSJ6rX1sIUE2zvUQetwPGhTmj-uwn07cOirGL0m5c9H7_HGiNIiWLtPFISDfUIf_jXR_ayykq_AhWtYHYx").get("https://api.yelp.com/v3/businesses/#{yelp_business_id}")
+    response = HTTP.auth("#{Rails.application.credentials.my_api_key}").get("https://api.yelp.com/v3/businesses/#{yelp_business_id}")
     api_business_object = response.parse(:json)
     return api_business_object
   #   return {
