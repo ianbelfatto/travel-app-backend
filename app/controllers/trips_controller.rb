48d707939f6c9cc3ba2abeb4421aa6e6c8194b4b
ianbelfatto <ibelfatto@gmail.com>
2021-06-18 14:48:19 -0400

    adds trips index show update destroy

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -13,5 +13,32 @@ class TripsController < ApplicationController
       render json: { errors: trip.errors.full_messages }, status: :bad_request
     end
   end
+
+  def index
+    trips = Trip.all
+    render json: trips.as_json
+  end
+
+  def show
+    trip = Trip.find_by(id: params[:id])
+    render json: trip
+  end
+
+  def update
+    trip = Trip.find_by(id: params[:id])
+    trip.name = params[:name] || trip.name
+    trip.city =params[:city] || trip.city
+    trip.state = params[:state] || trip.state
+    trip.image_url = params[:image_url] || trip.image_url
+    trip.save
+    render json: trip.as_json
+  end
+
+  def destroy
+    trip = Trip.find_by(id: params[:id])
+    trip.destroy
+    render json: {message: "Trip successfully deleted!"}
+  end
+
 end
 
