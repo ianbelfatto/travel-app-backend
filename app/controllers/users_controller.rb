b2b88e4030e6cb236e2b9ed51305c414f67db0bd
ianbelfatto <ibelfatto@gmail.com>
2021-06-18 14:48:35 -0400

    adds user show update destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :current_user
+
   def create
     user = User.new(
       first_name: params[:first_name],
@@ -12,5 +14,26 @@ class UsersController < ApplicationController
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user.as_json
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.first_name = params[:first_name] || user.first_name
+    user.last_name =params[:last_name] || user.last_name
+    user.email = params[:email] || user.email
+    user.password = params[:password] || user.password
+    user.save
+    render json: user.as_json
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+    user.destroy
+    render json: {message: "User successfully deleted!"}
   end
 end
