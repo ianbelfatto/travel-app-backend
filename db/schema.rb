5cdd1a39111c1ff13e8018528216094a2b2b25d4
ianbelfatto <ibelfatto@gmail.com>
2021-06-22 16:25:24 -0400

    creates businesses table (not needed)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_192304) do
+ActiveRecord::Schema.define(version: 2021_06_22_161835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "businesses", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "trip_businesses", force: :cascade do |t|
     t.integer "trip_id"
