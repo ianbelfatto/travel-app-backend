2ebea0e0e4af7db9853cb92a1b9176f939e2bed9
ianbelfatto <ibelfatto@gmail.com>
2021-06-22 16:23:22 -0400

    creates tripbusinesses controller

--- app/controllers/trip_businesses_controller.rb
+++ app/controllers/trip_businesses_controller.rb
@@ -1,2 +1,31 @@
 class TripBusinessesController < ApplicationController
+  before_action :authenticate_user
+  def create
+    trip_business = TripBusiness.new(
+      trip_id: params[:trip_id],
+      yelp_business_id: params[:yelp_business_id],
+      comments: params[:comments]
+    )
+    if trip_business.save
+      render json: { message: "TripBusiness Instance created successfully" }, status: :created
+    else
+      render json: { errors: trip_business.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    trip_business = TripBusiness.find_by(id: params[:id])
+    trip_business.comments = params[:comments]
+    if trip_business.save
+      render json: { message: "Trip updated successfully", upadted_trip_business: trip_business }, status: :created
+    else
+      render json: { errors: trip_business.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    trip_business = TripBusiness.find_by(id: params[:id])
+    trip_business.destroy
+    render json: {message: "Trip Business Instance successfully deleted!"}
+  end
 end
