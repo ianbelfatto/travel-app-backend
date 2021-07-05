248c2ef09eadfbd15bd6fece56b1c89fab7ede6b
ianbelfatto <ibelfatto@gmail.com>
2021-07-05 14:48:15 -0400

    organizes and confirms restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   # Users
-  get "/users/:id" => "users#show"
   post "/users" => "users#create"
+  get "/users/:id" => "users#show"
   patch "/users/:id" => "users#update"
   delete "/users/:id" => "users#destroy"
+
+  # Sessions
   post "/sessions" => "sessions#create"
 
   # Trips
@@ -14,12 +16,12 @@ Rails.application.routes.draw do
   patch "/trips/:id" => "trips#update"
   delete "/trips/:id" => "trips#destroy"
 
+  # Trip Businesses
   post "/tripbusiness" => "trip_businesses#create"
   patch "/tripbusiness/:id" => "trip_businesses#update"
   delete "/tripbusiness/:id" => "trip_businesses#destroy"
 
-
-  
+  # Businesses (from YELP API)
   get "/businesses/" => "businesses#index"
   get "/businesses/:yelp_business_id" => "businesses#show"
 end
