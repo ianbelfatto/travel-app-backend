c5246529b4ed2a54f10b878db551bb68e92c3c6e
ianbelfatto <ibelfatto@gmail.com>
2021-07-07 15:33:21 -0400

    starts backend business hours fix

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -19,6 +19,7 @@ class BusinessesController < ApplicationController
       location: api_business_object["location"]["display_address"],
       lat: api_business_object["coordinates"]["latitude"],
       lng: api_business_object["coordinates"]["longitude"],
+      hours: api_business_object["hours"]
     }
   end
 end
